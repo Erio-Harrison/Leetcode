11e6c9f49c554e259c00a6e020511a515a48fc7c
Haoren Hu <u7541840@anu.edu.au>
2023-06-14 17:57:04 +1000

    Update Lee328

---
+++ Leetcode/src/Lee328.java
@@ -0,0 +1,29 @@
+package src;
+
+public class Lee328{
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null||head.next==null||head.next.next==null) return head;
+        ListNode odd = head;
+        ListNode even =head.next;
+        ListNode evenPoint = head.next;
+
+        while(even!=null&&even.next!=null){
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
+        }
+
+        odd.next=evenPoint;
+        return odd;
+    }
+
+}
