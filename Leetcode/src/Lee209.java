65b649802092b5d19653018926f4a691c6c41a60
Haoren <u7541840@anu.edu.au>
2023-08-09 21:27:55 +1000

    update 209

--- Leetcode/src/Lee209.java
+++ Leetcode/src/Lee209.java
@@ -1,5 +1,20 @@
 package src;
 
 public class Lee209 {
-    //TODO
+    public int minSubArrayLen(int target, int[] nums) {
+        int length = Integer.MAX_VALUE;
+        int left=0,right=0,sum=0;
+
+        while(right<nums.length){
+            sum += nums[right];
+            right++;
+
+            while(sum>=target){
+                length = Math.min(length,right-left);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return length==Integer.MAX_VALUE ? 0:length;
+    }
 }
