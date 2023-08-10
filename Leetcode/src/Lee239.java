5af9e76588fcec9f937a597ffb408a0cebd00e64
Haoren <u7541840@anu.edu.au>
2023-08-10 17:52:00 +1000

    update 239 and record

--- Leetcode/src/Lee239.java
+++ Leetcode/src/Lee239.java
@@ -4,7 +4,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 public class Lee239 {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0) return new int[0];
 
         int n = nums.length;
@@ -17,16 +17,28 @@ public class Lee239 {
                 deque.pollFirst();
             }
 
+            // Remove indices of elements less than the current one
+            // This ensures the deque's first index always points to the max element
             while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
                 deque.pollLast();
             }
 
             deque.offerLast(i);
-            
+
+            // The first element in the deque is the max of the current window,
+            // add it to the result array
             if (i >= k - 1) {
                 result[i - k + 1] = nums[deque.peekFirst()];
             }
         }
+
         return result;
+    }
+
+    public static void main(String[] args) {
+        int[] x = maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7},3);
+        for(int i:x){
+            System.out.println(i);
+        }
     }
 }
