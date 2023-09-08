c9810b8f517a23f42264ab82727053a937c2a4a1
Haoren <u7541840@anu.edu.au>
2023-09-08 12:27:06 +1000

    update thoughts

--- Leetcode/src/Lee111.java
+++ Leetcode/src/Lee111.java
@@ -1,5 +1,8 @@
 package src;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Lee111 {
     public class TreeNode {
         int val;
@@ -13,16 +16,48 @@ public class Lee111 {
             this.right = right;
         }
     }
-    public int fun(TreeNode root){
+    public int depth(TreeNode root){
         if(root==null)
-            return 10000;
+            return 100000;
         if(root.left==null && root.right==null)
             return 1;
-        return 1+Math.min(fun(root.left),fun(root.right));
+        return 1+Math.min(depth(root.left),depth(root.right));
     }
     public int minDepth(TreeNode root) {
         if(root==null)
             return 0;
-        return fun(root);
+        return depth(root);
+    }
+
+    public int minDepth1(TreeNode root) {
+        if (root == null) return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+
+
+                if (currentNode.left == null && currentNode.right == null) {
+                    return depth;
+                }
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            depth++;
+        }
+
+        return depth;
     }
 }
